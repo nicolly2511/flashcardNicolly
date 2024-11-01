2bb8abe13cc53ae96c2d6ea32be497dcb7eeb3f5
nicolly2511 <164517162+nicolly2511@users.noreply.github.com>
2024-11-01 11:21:45 -0300

    Add files via upload

---
+++ flashcardNicolly/flashcard-aula_10/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Biologia',
+    'Quais são os cinco reinos dos seres vivos?',
+    'Monera, Protista, Fungi, Plantae e Animalia. '
+)
+
+criaCartao(
+    'Geografia',
+    'Qual continente tem mais países?',
+    'África'
+)
+
+criaCartao(
+    'História',
+    'Qual civilização construiu as pirâmides do Egito?',
+    'Os antigos egípicios'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Qual é o passado do verbo "go"?',
+    'Went'
+)
